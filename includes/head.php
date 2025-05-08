18abf722f6ca0834a7e372dfeec2a9b6cb388654
Ashuksu <ashuksu@gmail.com>
2025-05-09 01:47:40 +0200

    updated head

--- includes/head.php
+++ includes/head.php
@@ -1,29 +1,18 @@
-<?php
-define("APP_PATH", "/");
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+	<meta charset="UTF-8">
 	<meta name="viewport"
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="msapplication-TileColor" content="#d2d9f4">
-    <meta name="theme-color" content="#d2d9f4">
 
-	<title><?=APP_TITLE?></title>
+	<title><?= APP_TITLE ?></title>
+	<link rel="shortcut icon" href="<?= APP_PATH ?>favicon.ico">
 
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;900&display=swap">
 
-	<link rel="shortcut icon" href="<?=APP_PATH?>favicon.ico">
-	<link rel="stylesheet" href="<?=APP_PATH?>assets/css/style.css">
+	<link rel="stylesheet" href="<?= APP_PATH ?>assets/css/style.css">
 </head>
-
-<body data-style="default" class="<?=$bodyClass?>">
-	<div class="wrapper">
-		<div class="content">
-
-
 
