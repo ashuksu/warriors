a7ae7b98b8f8f5bd56edbedad27b5bd16616d4c4
Ashuksu <ashuksu@gmail.com>
2025-05-09 02:17:34 +0200

    added markup and stylized main-section

--- includes/sections/main-section.php
+++ includes/sections/main-section.php
@@ -1,3 +1,28 @@
 <section class="section main-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 mb-3 mb-md-0">
+				<div class="inner main-section__inner">
+					<h1 class="title main-section__title">
+						Lorem ipsum dolor sit amet
+					</h1>
+					<p class="main-section__text">
+						lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur
+					</p>
 
+					<?php
+					render_button([
+						'content' => 'Go',
+					]);
+					?>
+				</div>
+			</div>
+			<div class="col-md-6 mt-md-0">
+				<div class="image">
+					<img src="<?= APP_PATH ?>assets/images/w3r.svg" alt="image"
+						 class="" width="600" height="600">
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
