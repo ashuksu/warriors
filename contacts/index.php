1892c9ecd8d53270f5ea3c30ee1f62e703b31962
Ashuksu <ashuksu@gmail.com>
2025-05-09 02:48:31 +0200

    made Contacts-page

---
+++ contacts/index.php
@@ -0,0 +1,8 @@
+<?php
+define("APP_TITLE", "Contacts Page");
+$dir = "../";
+$bodyClass = 'page-contacts';
+$sections = ['contacts', 'info'];
+
+include $dir . 'includes/Layout.php';
+?>
---
+++ includes/sections/contacts.php
@@ -0,0 +1,28 @@
+<section class="section contacts">
+	<div class="container">
+		<div class="inner">
+			<h1 class="title text-center mb-5 wow pixFadeUp" data-wow-delay="0.2s">
+				Contacts
+			</h1>
+			<div class="row contacts__list wow pixFadeUp" data-wow-delay="0.3s">
+				<?php
+				$contacts = [
+					[
+						'title' => 'Email',
+						'text' => 'lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ',
+						'url' => 'mailto:' . $EMAIL,
+					],
+					[
+						'title' => 'Telegram',
+						'text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda consequatur dolore eum fuga itaque quasi qui quisquam sequi tenetur.',
+						'url' => 'tel:' . $PHONE,
+					]
+				];
+				foreach ($contacts as $index => $item) {
+					include $dir . 'includes/components/contacts-card.php';
+				}
+				?>
+			</div>
+		</div>
+	</div>
+</section>
