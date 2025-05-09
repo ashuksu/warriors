f308dad8594b73078b3f500aacfd75a03aa81830
Ashuksu <ashuksu@gmail.com>
2025-05-09 02:09:14 +0200

    added logo, added murkup and styles for header, footer

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,9 @@
 <header id="header" class="header">
-
+	<div class="container">
+		<div class="header__inner">
+			<a href="<?= APP_PATH ?>" class="logo">
+				<img src="<?= APP_PATH ?>assets/images/logo/logo-1.png" width="30" height="30" alt="logo">
+			</a>
+		</div>
+	</div>
 </header>
