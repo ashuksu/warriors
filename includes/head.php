3c6c64af309289b8ca8950238862cbe2d456e0ec
Ashuksu <ashuksu@gmail.com>
2025-05-09 02:14:17 +0200

    added jq, bootstrap cdn-links to the project

--- includes/head.php
+++ includes/head.php
@@ -13,6 +13,7 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;900&display=swap">
 
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?= APP_PATH ?>assets/css/style.css">
 </head>
 
