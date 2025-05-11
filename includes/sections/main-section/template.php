ac839ea55e3b4232adaa3d5c2d231bc59ac02bc0
Ashuksu <ashuksu@gmail.com>
2025-05-11 04:32:00 +0200

    Refactored styles for banner-images, checked main-section markup

--- includes/sections/main-section/template.php
+++ includes/sections/main-section/template.php
@@ -1,7 +1,7 @@
 <section class="section main-section">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-5 mb-3 mb-md-0">
+			<div class="col-lg-5 col-md-6 mb-3 mb-md-0">
 				<div class="inner main-section__inner">
 					<h1 class="title main-section__title">
 						Lorem ipsum dolor sit amet
