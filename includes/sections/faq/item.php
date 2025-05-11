e663e2f361fef82c342a0760edc60292b6e83e6c
Ashuksu <ashuksu@gmail.com>
2025-05-11 14:12:45 +0200

    Rename `$isActive` to `$setActiveClass` for clarity

--- includes/sections/faq/item.php
+++ includes/sections/faq/item.php
@@ -1,5 +1,5 @@
 <?php
-	$isActive = $index === 0 ? 'active' : '';
+	$setActiveClass = $index === 0 ? 'active' : '';
 ?>
 
 <div class="faq__item text-justify wow pixFadeUp" data-wow-delay="0.3s">
@@ -9,13 +9,13 @@
 		<?php
 		render_button([
 			'url' => 'faq-content-' . $index,
-			'class' => 'faq__item-button button--plus button--transparent ' . $isActive,
+			'class' => 'faq__item-button button--plus button--transparent ' . $setActiveClass,
 			'attr' => 'data-button="toggle"',
 			'content' => 'Open',
 		]);
 		?>
 	</h3>
-	<div id="faq-content-<?= $index ?>" class="faq__item-inner <?= $isActive ?>">
+	<div id="faq-content-<?= $index ?>" class="faq__item-inner <?= $setActiveClass ?>">
 		<div class="faq__item-text">
 			<?= $item['text'] ?>
 		</div>
