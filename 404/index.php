32a46f5e98b7253cb75ff303d03afd7e57c5350f
Ashuksu <ashuksu@gmail.com>
2025-05-16 00:47:44 +0200

    Add error section to JSON and update error handling in templates

--- 404/index.php
+++ 404/index.php
@@ -4,5 +4,11 @@ require_once __DIR__ . '/../config.php';
 $bodyClass = 'page-error';
 $sections = ['error'];
 
+require_once PROJECT_ROOT . 'Services/SectionService.php';
+$title = SectionService::get('error', 'title');
+$text = SectionService::get('error', 'text');
+$image = SectionService::get('error', 'image');
+$button = SectionService::get('error', 'button');
+
 include PROJECT_ROOT . 'includes/Layout.php';
 ?>
--- includes/sections/error/template.php
+++ includes/sections/error/template.php
@@ -2,23 +2,22 @@
     <div class="container">
         <div class="inner error__inner text-center">
             <h1 class="title mb-5 wow pixFadeUp" data-wow-delay="0.2s">
-                404
+                <?= $title ?>
             </h1>
             <p class="error__text error__text wow pixFadeUp" data-wow-delay="0.3s">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum cupiditate dicta earum enim ipsum iure
-                pariatur porro possimus
+                <?= $text ?>
             </p>
             <div class="image error__image mb-5 mx-auto wow pixFadeUp" data-wow-delay="0.4s">
-                <img src="<?= APP_PATH ?>assets/images/w4.gif" alt="image"
-                     class="error-image" width="600" height="600">
+                <img src="<?= APP_PATH ?>assets/images/<?= $image['name'] ?>"  alt="<?= $image['alt'] ?>"
+                     class="error-image" width="<?= $image['width'] ?>" height="<?= $image['height'] ?>">
             </div>
 
             <?php
             render_button([
-                'url' => APP_PATH,
-                'class' => 'error-button wow pixFadeUp',
-                'attr' => 'data-wow-delay="0.5s" target="_blank"',
-                'content' => 'to the Home page',
+                'url' => ($button['url'] === '/') ? APP_PATH : "/",
+                'class' => $button['class'],
+                'attr' => $button['attr'],
+                'content' => $button['content']
             ]);
             ?>
         </div>
