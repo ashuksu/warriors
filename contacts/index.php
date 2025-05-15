b528927e9874b5446781730777908b288d1cf1a8
Ashuksu <ashuksu@gmail.com>
2025-05-16 00:20:09 +0200

    improve contacts section layout and structure

--- contacts/index.php
+++ contacts/index.php
@@ -4,5 +4,9 @@ require_once __DIR__ . '/../config.php';
 $bodyClass = 'page-contacts';
 $sections = ['contacts', 'info'];
 
+require_once PROJECT_ROOT . 'Services/SectionService.php';
+$title = SectionService::get('contacts', 'title');
+$contacts = SectionService::get('contacts', 'items');
+
 include PROJECT_ROOT . 'includes/Layout.php';
 ?>
--- includes/sections/contacts/template.php
+++ includes/sections/contacts/template.php
@@ -2,24 +2,13 @@
     <div class="container">
         <div class="inner">
             <h1 class="title text-center mb-5 wow pixFadeUp" data-wow-delay="0.2s">
-                Contacts
+                <?= $title ?>
             </h1>
+
             <div class="row contacts__list wow pixFadeUp" data-wow-delay="0.3s">
                 <?php
-                $contacts = [
-                    [
-                        'title' => 'Email',
-                        'text' => 'lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ',
-                        'url' => 'mailto:' . $EMAIL,
-                    ],
-                    [
-                        'title' => 'Telegram',
-                        'text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda consequatur dolore eum fuga itaque quasi qui quisquam sequi tenetur.',
-                        'url' => $TELEGRAM,
-                    ]
-                ];
                 foreach ($contacts as $index => $item) {
-                    include PROJECT_ROOT . 'includes/sections/contacts/item.php';
+                    include __DIR__ . '/item.php';
                 }
                 ?>
             </div>
