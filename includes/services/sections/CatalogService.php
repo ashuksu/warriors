43b41fdf94548d7c0b0a9091652ea4de4a1e332f
Ashuksu <ashuksu@gmail.com>
2025-05-15 19:19:12 +0200

    Refactor CatalogService methods to streamline data retrieval

--- includes/services/sections/CatalogService.php
+++ includes/services/sections/CatalogService.php
@@ -37,29 +37,8 @@ class CatalogService extends BaseSectionService
      * @param string $type Type of data to return ('items', 'title', etc.)
      * @return mixed Catalog data based on requested type
      */
-    public function getCatalog($type = 'items')
+    public function getData($type = 'items')
     {
         return $this->getSectionData('catalog', $type);
     }
-
-    /**
-     * Get catalog items with caching (legacy method)
-     *
-     * @return array Catalog items
-     */
-    public function getCatalogItems()
-    {
-        return $this->getCatalog('items');
-    }
-
-    /**
-     * Get catalog title with caching (legacy method)
-     *
-     * @return string Catalog title
-     */
-    public function getCatalogTitle()
-    {
-        return $this->getCatalog('title');
-    }
-
 }
