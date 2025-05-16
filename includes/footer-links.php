848a3aa7754fda88e58d61dbc923e2239b3c5dc2
Ashuksu <ashuksu@gmail.com>
2025-05-16 10:07:11 +0200

    Remove unused Bootstrap JS from footer links

--- includes/footer-links.php
+++ includes/footer-links.php
@@ -1,5 +1,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/wowjs@1.1.3/dist/wow.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.x.x/dist/js/bootstrap.bundle.min.js"></script>
 
 <script type="module" src="<?= APP_PATH ?>assets/js/script.js"></script>
