0deb5d2b5e15f338593a1de278c0f3b2804a4aff
Ashuksu <ashuksu@gmail.com>
2025-05-16 12:02:17 +0200

    Refactor "info" section to support page-specific items.

--- includes/sections/info/template.php
+++ includes/sections/info/template.php
@@ -1,17 +1,18 @@
 <?php
-$title = SectionService::get('info', 'title');
-$text = SectionService::get('info', 'text');
+$item = SectionService::get('info', 'items', 'info-' . PAGE);
 ?>
 
-<section id="info" class="section info mt-auto">
-    <div class="container">
-        <div class="inner inner-style text-center info__inner wow pixFadeUp" data-wow-delay="0.2s">
-            <h2 class="title">
-                <?= $title ?>
-            </h2>
-            <p class="text-justify">
-                <?= $text ?>
-            </p>
+<?php if (!empty($item) && is_array($item)): ?>
+    <section id="info" class="section info mt-auto">
+        <div class="container">
+            <div class="inner inner-style text-center info__inner wow pixFadeUp" data-wow-delay="0.2s">
+                <h2 class="title">
+                    <?= $item['title'] ?>
+                </h2>
+                <p class="text-justify">
+                    <?= $item['text'] ?>
+                </p>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
