838dd9b141109fd2c08c5a7fc6b52a0af0709db4
Ashuksu <ashuksu@gmail.com>
2025-05-16 02:28:24 +0200

    Add dynamic rendering for "info" section content

--- includes/sections/info/template.php
+++ includes/sections/info/template.php
@@ -1,18 +1,16 @@
+<?php
+$title = SectionService::get('info', 'title');
+$text = SectionService::get('info', 'text');
+?>
+
 <section id="info" class="section info mt-auto">
     <div class="container">
         <div class="inner inner-style text-center info__inner wow pixFadeUp" data-wow-delay="0.2s">
             <h2 class="title">
-                Lorem ipsum dolor sit amet
+                <?= $title ?>
             </h2>
             <p class="text-justify">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid asperiores assumenda aut autem
-                blanditiis debitis delectus deleniti dolorem eveniet harum id inventore minus modi molestias
-                necessitatibus nisi nostrum quaerat quas qui quidem, quo, quod reprehenderit sunt temporibus, tenetur
-                totam ut veniam. Commodi delectus nihil nostrum rerum veritatis? Animi, beatae cum cupiditate eligendi
-                ex excepturi harum ipsum minus nostrum odio? Accusantium asperiores aspernatur beatae doloribus iusto
-                magnam magni minima officiis quisquam, tempore. Accusantium asperiores et fuga harum in incidunt ipsa,
-                iusto magni mollitia nostrum perferendis quas quos reprehenderit rerum sapiente sequi temporibus, ullam
-                unde, veritatis voluptates? Deleniti et hic necessitatibus quod.
+                <?= $text ?>
             </p>
         </div>
     </div>
