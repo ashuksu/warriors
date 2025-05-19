3e46c3479da4b6d77826b28dcd38f6782268415d
Ashuksu <ashuksu@gmail.com>
2025-05-19 18:13:20 +0200

    feature/update - refactored Info section to use renderTemplate for improved rendering and data retrieval

--- app/Views/Sections/Info/Info.php
+++ app/Views/Sections/Info/Info.php
@@ -2,29 +2,18 @@
 
 namespace Views\Sections\Info;
 
+use Services\SectionService;
+use function Helpers\renderTemplate;
+
 class Info
 {
     public static function render($params = [])
     {
         extract($params);
 
-        if (!empty($info) && is_array($info)):
-            ?>
-
-            <section id="info" class="section info mt-auto">
-                <div class="container">
-                    <div class="inner inner-style text-center info__inner wow pixFadeUp" data-wow-delay="0.2s">
-                        <h2 class="title">
-                            <?= $info['title'] ?>
-                        </h2>
-                        <p class="text-justify">
-                            <?= $info['text'] ?>
-                        </p>
-                    </div>
-                </div>
-            </section>
-
-        <?php
-        endif;
+        renderTemplate(__DIR__ . '/template.php', [
+            'item' => SectionService::get('info', 'items', 'info-' . PAGE),
+            'section' => 'info',
+        ]);
     }
 }
