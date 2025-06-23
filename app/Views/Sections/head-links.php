474f66385a8d4cc1e8ef3b3da59b529816d55aad
Ashuksu <ashuksu@gmail.com>
2025-06-23 16:46:44 +0200

    WRRS-refactoring - Refactored head-links.php to utilize ViteService for asset path resolution, enhancing consistency and maintainability across styles and scripts.

--- app/Views/Sections/head-links.php
+++ app/Views/Sections/head-links.php
@@ -1,9 +1,3 @@
-<?php
-
-use App\Helpers\Vite;
-
-?>
-
 <!-- Preconnect to external domains to improve performance -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -15,37 +9,37 @@ use App\Helpers\Vite;
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;900&display=swap">
 </noscript>
 
-
-<?php if (defined('IS_DEV') && IS_DEV): ?>
+<?php if ($configService->get('is_dev')): ?>
 	<!-- Vite client (only in dev mode) -->
-	<script type="module" src="<?= VITE_DEV_CLIENT ?>"></script>
+	<script type="module" src="<?= $configService->get('vite.client') ?>"></script>
 <?php endif; ?>
 
-
 <!-- Styles: critical (loaded immediately, blocks' rendering) -->
-<link rel="stylesheet" href="<?= Vite::getAssetPath('src/styles/critical.scss') ?>">
+<link rel="stylesheet" href="<?= $viteService->getAssetPath('src/styles/critical.scss') ?>">
 
 <!-- Styles: main, not important and interactive elements, popup (async loaded, lower priority than "high", be applied before "high") -->
-<link rel="preload" href="<?= Vite::getAssetPath('src/styles/main.scss') ?>" as="style" onload="this.rel='stylesheet'"
+<link rel="preload" href="<?= $viteService->getAssetPath('src/styles/main.scss') ?>" as="style" onload="this.rel='stylesheet'"
 	  fetchpriority="low">
 <noscript>
-	<link rel="stylesheet" href="<?= Vite::getAssetPath('src/styles/main.scss') ?>">
+	<link rel="stylesheet" href="<?= $viteService->getAssetPath('src/styles/main.scss') ?>">
 </noscript>
 
 <!-- Styles: page, important elements (async loaded, higher priority than "low", be applied after "low") -->
-<link rel="preload" href="<?= Vite::getAssetPath('src/styles/page_home.scss') ?>" as="style"
+<!-- TODO: $viteService->getAssetPath('src/styles/page_' . $pageDataName . '.scss') -->
+<link rel="preload" href="<?= $viteService->getAssetPath('src/styles/page_home.scss') ?>" as="style"
 	  onload="this.rel='stylesheet'">
+<!-- TODO: $viteService->getAssetPath('src/styles/page_' . $pageDataName . '.scss') -->
 <noscript>
-	<link rel="stylesheet" href="<?= Vite::getAssetPath('src/styles/page_home.scss') ?>">
+	<link rel="stylesheet" href="<?= $viteService->getAssetPath('src/styles/page_home.scss') ?>">
 </noscript>
 
 <!-- Styles: first-screen, important elements (blocks rendering) -->
-<link rel="stylesheet" href="<?= Vite::getAssetPath('src/styles/page_home_critical.scss') ?>">
-
+<!--- TODO: $viteService->getAssetPath('src/styles/page_' . $pageDataName . '_critical.scss') -->
+<link rel="stylesheet" href="<?= $viteService->getAssetPath('src/styles/page_home_critical.scss') ?>">
 
 <!-- Prefetch resources that will be needed soon -->
-<link rel="prefetch" href="<?= Vite::getAssetPath('src/js/modules/Popup.js') ?>" as="script">
-<link rel="prefetch" href="<?= Vite::getAssetPath('src/js/modules/utils/Toggle.js') ?>" as="script">
+<link rel="prefetch" href="<?= $viteService->getAssetPath('src/js/modules/Popup.js') ?>" as="script">
+<link rel="prefetch" href="<?= $viteService->getAssetPath('src/js/modules/utils/Toggle.js') ?>" as="script">
 
 <!-- Load WOW.js with defer and low priority to ensure it doesn't block critical resources -->
 <script defer src="https://cdn.jsdelivr.net/npm/wowjs@1.1.3/dist/wow.min.js" fetchpriority="low"></script>
